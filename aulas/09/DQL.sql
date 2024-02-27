dc038f30a8cdcc16a2da118887f516668e22331c
luisgrbenvindo@gmail.com <luisgrbenvindo@gmail.com>
2024-02-26 22:18:42 -0300

    Inicio DQL

---
+++ aulas/09/DQL.sql
@@ -0,0 +1,9 @@
+SELECT * FROM ALUNO;
+
+SELECT NOME FROM ALUNO ORDER BY NOME;
+
+SELECT A.nome from Aluno a;
+
+SELECT PF.professor_ID FROM professores_disciplinas PF;
+
+SELECT Nome as NM FROM ALUNO;
